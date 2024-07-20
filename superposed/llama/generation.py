e0008d1a91710b8b00b0b0d14cd0b7bff1d53111
Avinash Amballa <amballaavinash@gmail.com>
2024-07-20 02:14:36 -0400

    beam search

--- superposed/llama/generation.py
+++ superposed/llama/generation.py
@@ -307,8 +307,12 @@ def sample_top_p(probs, p, s=1):
     mask = probs_sum - probs_sort > p
     probs_sort[mask] = 0.0
     probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
-    print(probs_sort.shape)
-    next_token = torch.multinomial(probs_sort, num_samples=s)
-    next_token = torch.gather(probs_idx, -1, next_token)
+    batch_size, beam_size, vocab_size = probs_sort.shape
+    # next_token = torch.multinomial(probs_sort, num_samples=s)
+    # next_token = torch.gather(probs_idx, -1, next_token)
+    next_token = torch.multinomial(probs_sort.view(-1, vocab_size), num_samples=1).view(batch_size, beam_size)
+    next_token = torch.gather(probs_idx, -1, next_token.unsqueeze(-1)).squeeze(-1)
+    
+
     unsorted = probs_sort.gather(-1,probs_idx.argsort(-1))
     return next_token, unsorted
